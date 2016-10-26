571d3649cef872ba74cc9e12ec924019af2fa736
Amine Sagaama <amine.sagaama@gmail.com>
2016-10-26 22:26:03 +0200

    first commit

---
+++ src/main/java/com.asagaama.microservices.model/Calculation.java
@@ -0,0 +1,7 @@
+package com.asagaama.microservices.model;
+
+/**
+ * Created by asagaama on 26/10/2016.
+ */
+public class Calculation {
+}
---
+++ src/main/java/com/asagaama/microservices/MicroservicesProjectApplication.java
@@ -0,0 +1,12 @@
+package com.asagaama.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesProjectApplication.class, args);
+	}
+}
